6249f4c9fbd7202f719b72b0ea1a38ff01923477
udjin <udjinskobelev@gmail.com>
2018-05-08 13:20:08 +0300

    #1 init project

--- back/src/main/java/org/webtree/trust/boot/App.java
+++ back/src/main/java/org/webtree/trust/boot/App.java
@@ -1,9 +1,11 @@
-package org.webtree.trust.trustapi.back.boot;
+package org.webtree.trust.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.webtree.trust")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
